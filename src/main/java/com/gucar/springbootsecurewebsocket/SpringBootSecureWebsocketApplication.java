b2a56e3563864a8088792850c8c9beebb1e526e6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-10 13:38:11 +0300

    project initialized

---
+++ src/main/java/com/gucar/springbootsecurewebsocket/SpringBootSecureWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.gucar.springbootsecurewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecureWebsocketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecureWebsocketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gucar/springbootsecurewebsocket/SpringBootSecureWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucar.springbootsecurewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecureWebsocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
